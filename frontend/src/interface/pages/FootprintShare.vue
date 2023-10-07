40e45dda1fd1e066d3617b871059a5f9ff94167b
Vick Scarlet <scarlet_vick@outlook.com>
2023-10-07 15:31:25 +0800

    update: tooltips

--- frontend/src/interface/pages/FootprintShare.vue
+++ frontend/src/interface/pages/FootprintShare.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { getShare } from '@/api/mark'
-import { size } from '@/utils/geo'
+import { size, info } from '@/utils/geo'
 import MapComponent from ':/components/MapComponent.vue'
 
 const props = defineProps<{
@@ -14,11 +14,21 @@ getShare(props.id).then(({ success, data }) => {
   for (const [index, value] of data)
     values.value[index] = value
 })
+const tooltipValues = [
+  '还未标记过',
+  '想去',
+  '路过',
+  '去过',
+  '住过',
+  '家乡'
+]
+const tooltip = (index: number) => `<strong>${info(index)?.fullname}</strong><br />${tooltipValues[values.value[index]]}`
 </script>
 
 <template>
   <MapComponent
     :max="5"
     :values="values"
+    :tooltip="tooltip"
   />
 </template>
