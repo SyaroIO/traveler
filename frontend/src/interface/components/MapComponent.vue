320116b43d0f57759d18cababf32779387617534
Vick Scarlet <scarlet_vick@outlook.com>
2023-10-07 12:34:18 +0800

    add: save image

--- frontend/src/interface/components/MapComponent.vue
+++ frontend/src/interface/components/MapComponent.vue
@@ -3,14 +3,16 @@ import { ref, computed } from 'vue'
 import * as geo from '@/utils/geo'
 import VChart from 'vue-echarts'
 import type { ECBasicOption } from 'echarts/types/dist/shared'
+import type { ECharts } from 'echarts'
 import { rgb, hex, merge } from '@/utils/common'
-import { View, Hide } from '@element-plus/icons-vue'
+import { View, Hide, Download } from '@element-plus/icons-vue'
 
 const props = defineProps<{
   values: number[],
   max: number,
 }>()
 const $emit = defineEmits(['district-click'])
+const chart = ref<ECharts | null>(null)
 const show = ref(true)
 const color = ref("#0ff")
 const zoom = ref(1)
@@ -97,42 +99,79 @@ const roam = (e: { dx?: number, dy?: number, zoom?: number }) => {
   scale(e.zoom);
 }
 
+const download = () => {
+  const url = chart.value?.getDataURL({
+    type: 'png',
+    pixelRatio: 1.5,
+    backgroundColor: '#121212'
+  });
+  if (!url) return;
+  const elink = document.createElement('a');
+  elink.download = "map";
+  elink.style.display = 'none';
+  elink.href = url;
+  document.body.appendChild(elink);
+  elink.click();
+  URL.revokeObjectURL(elink.href);
+  document.body.removeChild(elink)
+}
 </script>
 
 <template>
   <teleport to="#fabtl">
-    <el-tooltip
-      v-if="show"
-      content="隐藏地名"
-      placement="right"
-    >
-      <el-button
-        circle
-        type="primary"
+    <el-row>
+      <el-tooltip
+        v-if="show"
+        content="隐藏地名"
+        placement="right"
+      >
+        <el-button
+          circle
+          type="primary"
+          size="large"
+          :icon="Hide"
+          @click="show = false"
+        />
+      </el-tooltip>
+      <el-tooltip
+        v-else
+        content="显示地名"
+        placement="right"
+      >
+        <el-button
+          circle
+          type="primary"
+          size="large"
+          :icon="View"
+          @click="show = true"
+        />
+      </el-tooltip>
+    </el-row>
+
+
+    <el-row>
+      <el-tooltip
+        content="保存图片"
+        placement="right"
+      >
+        <el-button
+          circle
+          type="primary"
+          size="large"
+          :icon="Download"
+          @click="download"
+        />
+      </el-tooltip>
+    </el-row>
+    <el-row>
+      <el-color-picker
+        v-model="color"
         size="large"
-        :icon="Hide"
-        @click="show = false"
       />
-    </el-tooltip>
-    <el-tooltip
-      v-else
-      content="显示地名"
-      placement="right"
-    >
-      <el-button
-        circle
-        type="primary"
-        size="large"
-        :icon="View"
-        @click="show = true"
-      />
-    </el-tooltip>
-    <el-color-picker
-      v-model="color"
-      size="large"
-    />
+    </el-row>
   </teleport>
   <v-chart
+    ref="chart"
     autoresize
     :option="option"
     :update-options="{ lazyUpdate: true }"
