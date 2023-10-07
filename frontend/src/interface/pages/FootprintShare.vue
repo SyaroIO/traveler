378d57d8d35001a1619374549a3352c6ea1ec6b2
Vick Scarlet <scarlet_vick@outlook.com>
2023-10-07 15:30:49 +0800

    update: tooltips

--- frontend/src/interface/pages/FootprintShare.vue
+++ frontend/src/interface/pages/FootprintShare.vue
@@ -14,11 +14,21 @@ getShare(props.id).then(({ success, data }) => {
   for (const [index, value] of data)
     values.value[index] = value
 })
+const tooltipValues = [
+  '还未标记过',
+  '想去',
+  '路过',
+  '去过',
+  '住过',
+  '家乡'
+]
+const tooltip = (index: number) => `<strong>${info(index)?.fullname}</strong><br />${tooltipValues[values.value[index]]}`
 </script>
 
 <template>
   <MapComponent
     :max="5"
     :values="values"
+    :tooltip="tooltip"
   />
 </template>
